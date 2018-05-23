2e6cceec1fedb090f906e955ec78c14658080e4d
Aaron Lobo <lobo.aaron1@gmail.com>
2018-05-23 18:30:40 +0200

    Implement finance pipeline

---
+++ pipelines/FinancePipeline.py
@@ -0,0 +1,38 @@
+import logging
+import pandas as pd
+from pathlib import Path
+from sqlalchemy import create_engine
+from pipelines.Pipeline import Pipeline
+from parsers.finance.FinanceParser import FinanceParser
+from parsers.finance.WatcardParser import WatcardParser
+
+class FinancePipeline(Pipeline):
+
+    DATA_SOURCE_FINANCE = 'data/finance/bank.tsv'
+    DATA_SOURCE_WATCARD = 'data/uwaterloo/watcard/watcard.htm'
+
+    logger = logging.getLogger(__name__)
+
+    def __init__(self):
+        Pipeline.__init__(self)
+
+    def extract(self):
+        self.logger.info('Extract')
+        assert(Path(FinancePipeline.DATA_SOURCE_FINANCE).is_file())
+        assert(Path(FinancePipeline.DATA_SOURCE_WATCARD).is_file())
+
+    def transform(self):
+        self.logger.info('Transforming Finances')
+        fp_parser = FinanceParser(FinancePipeline.DATA_SOURCE_FINANCE)
+        self.finances = fp_parser.parse()
+
+        wp_parser = WatcardParser(FinancePipeline.DATA_SOURCE_WATCARD)
+        self.finances = self.finances.append(wp_parser.parse())
+
+        self.finances = self.finances.sort_values(by='date')
+
+    def load(self):
+        self.logger.info('Loading Finances')
+        engine = create_engine(self.secrets['mysql']['connector'])
+        with engine.connect() as conn, conn.begin():
+            self.finances.to_sql(name='finances', con=engine, if_exists = 'replace', index=False)
