091fd367fcd439f65fb42cf5b8bf993137105beb
Aaron Lobo <lobo.aaron1@gmail.com>
2018-05-23 17:57:07 +0200

    Ensure important dates start and end are inclusive

--- pipelines/AcademicsPipeline.py
+++ pipelines/AcademicsPipeline.py
@@ -32,6 +32,11 @@ class AcademicsPipeline(Pipeline):
 
         self.logger.info('Processing Important Dates')
         self.important_dates = pd.read_csv(AcademicsPipeline.DATA_SOURCE_IMPORTANT_DATES)
+        # Ensure we include the last date
+        self.important_dates['start'] += ' 00:00:00'
+        self.important_dates['end'] += ' 23:59:59'
+        self.important_dates['start'] = pd.to_datetime(self.important_dates['start'])
+        self.important_dates['end'] = pd.to_datetime(self.important_dates['end'])
 
         self.logger.info('Processing Transcript')
         transcript_parser = TranscriptParser(AcademicsPipeline.DATA_SOURCE_TRANSCRIPT)
