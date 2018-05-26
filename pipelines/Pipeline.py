31bcdf81d86f6a3e8c7642cef5707af01431f55d
Aaron Lobo <lobo.aaron1@gmail.com>
2018-05-25 20:06:12 -0400

    Add SQL view creation support to Pipeline class

--- pipelines/Pipeline.py
+++ pipelines/Pipeline.py
@@ -4,11 +4,28 @@ from abc import ABCMeta, abstractmethod
 
 class Pipeline(metaclass=ABCMeta):
 
+    VIEW_DEFINITION = None
+    VIEW_DEFINITION_SQL = None
+
     def __init__(self):
         logger = logging.getLogger(__name__)
         logger.info('Loading Secrets')
         with open('data/secrets.json') as f:
             self.secrets = json.load(f)
+
+        # If this pipeline creates views, preprocess the SQL
+        # This is quite a hack, but I don't want to convert my SQL to sqlalchmey statements
+        if self.VIEW_DEFINITION:
+            with open(self.VIEW_DEFINITION) as f:
+                lines = f.readlines()
+
+            # string processing to remove comments and control characters
+            lines = filter(lambda x: '--' not in x, lines)
+            lines = ''.join(lines).replace('\n', ' ').replace('\t', ' ').split(';')
+            lines = map(lambda x: x.strip(), lines)
+            lines = filter(len, lines)
+            lines = map(lambda x: x + ';', lines)
+            self.VIEW_DEFINITION_SQL = ''.join(lines)
 
     @abstractmethod
     def extract(self):
