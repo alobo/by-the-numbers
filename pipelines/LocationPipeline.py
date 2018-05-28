eeb9d9842c66b7e53dc24b2ad41b448336002838
Aaron Lobo <lobo.aaron1@gmail.com>
2018-05-28 18:22:48 -0400

    Load important locations into DB

--- pipelines/LocationPipeline.py
+++ pipelines/LocationPipeline.py
@@ -8,6 +8,7 @@ from parsers.google.LocationHistoryParser import LocationHistoryParser
 class LocationPipeline(SQLPipeline):
 
     DATA_SOURCE_LOCATION = 'data/google/Location History/Location History.json'
+    DATA_SOURCE_IMPORTANT_LOCATIONS = 'data/important_locations.csv'
 
     VIEW_DEFINITION = 'views/location.sql'
 
@@ -19,11 +20,13 @@ class LocationPipeline(SQLPipeline):
     def extract(self):
         self.logger.info('Extract')
         assert(Path(LocationPipeline.DATA_SOURCE_LOCATION).is_file())
+        assert(Path(LocationPipeline.DATA_SOURCE_IMPORTANT_LOCATIONS).is_file())
 
     def transform(self):
         self.logger.info('Transforming Location History')
         lh_parser = LocationHistoryParser(LocationPipeline.DATA_SOURCE_LOCATION)
         self.location = lh_parser.parse()
+        self.important_locations = pd.read_csv(LocationPipeline.DATA_SOURCE_IMPORTANT_LOCATIONS)
 
     def load(self):
         self.logger.info('Loading Location History')
@@ -31,4 +34,5 @@ class LocationPipeline(SQLPipeline):
         with engine.connect() as conn, conn.begin():
             engine.execute('DROP TABLE IF EXISTS location;')
             self.location.to_sql(name='location', con=engine, if_exists = 'replace', index=False)
+            self.important_locations.to_sql(name='important_locations', con=engine, if_exists = 'replace', index=False)
             SQLPipeline.executeSQL(engine, self.VIEW_DEFINITION)
