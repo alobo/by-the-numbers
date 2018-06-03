ed7b7b01c5f6d75079e025811e41d3d57c8cdef3
Aaron Lobo <lobo.aaron1@gmail.com>
2018-06-03 19:25:19 -0400

    Remove unnecessary metrics

--- sql/academics.sql
+++ sql/academics.sql
@@ -9,13 +9,11 @@ GROUP BY term;
 DROP VIEW IF EXISTS academics_class_hours;
 CREATE VIEW academics_class_hours AS
 SELECT term,
+    SUM(CASE WHEN type = "LEC" or type = "PRJ" or type = "SEM" or type = "TUT" or type = "LAB" then duration END) / 60 as academic_hours,
     SUM(CASE WHEN type = "LEC" or type = "PRJ" or type = "SEM" then duration END) / 60 as class_hours,
-	AVG(CASE WHEN type = "LEC" or type = "PRJ" or type = "SEM" then duration END) / 60 as class_hours_avg,
-    COUNT(CASE WHEN type = "LEC" or type = "PRJ" or type = "SEM" then 1 END) as class_events,
+	AVG(CASE WHEN type = "LEC" or type = "PRJ" or type = "SEM" then duration END) / 60 as avg_lec_len,
     SUM(CASE WHEN type = "TUT" then duration END) / 60 as tut_hours,
-    COUNT(CASE WHEN type = "TUT" then 1 END) as tut_events,
     SUM(CASE WHEN type = "LAB" then duration END) / 60 as lab_hours,
-    COUNT(CASE WHEN type = "LAB" then 1 END) as lab_events
 FROM schedule
 LEFT JOIN important_dates ON schedule.start BETWEEN important_dates.start AND important_dates.end
 GROUP BY term;
