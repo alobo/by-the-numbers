75a1b3d7ec86233961ecf78f6d6e1ab248d83fcc
Aaron Lobo <lobo.aaron1@gmail.com>
2018-06-20 18:14:48 -0400

    Finance - Categorize coffee purchases

--- sql/finance.sql
+++ sql/finance.sql
@@ -1,12 +1,18 @@
+-- Categorize coffee purchases
+UPDATE finances
+SET category = 'Personal:Dining:Coffee'
+WHERE (
+	LOWER(description) LIKE "%hortons%" OR
+    LOWER(description) LIKE "%starbucks%" OR
+    LOWER(description) LIKE "%settlement%" OR
+    LOWER(description) LIKE "%coffee%"
+);
+
 -- Aggregate all transactions related to coffeeshops
 DROP VIEW IF EXISTS finance_coffee_spend;
 CREATE VIEW finance_coffee_spend AS
-SELECT term, SUM(amount) AS coffee_spend
+SELECT term, semester, SUM(amount) AS coffee_spend
 FROM finances
 LEFT JOIN important_dates ON finances.date BETWEEN important_dates.start AND important_dates.end
-WHERE amount < 0 AND (
-	LOWER(description) LIKE "%hortons%" OR
-    LOWER(description) LIKE "%starbucks%" OR
-    LOWER(description) LIKE "%coffee%"
-)
-GROUP BY term;
+WHERE amount < 0 AND LOWER(category) LIKE "%coffee%"
+GROUP BY term, semester;
