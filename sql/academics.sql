7f26f680cf5589f0c4d16e9ff3f0bd212a2e233d
Aaron Lobo <lobo.aaron1@gmail.com>
2018-06-20 18:15:20 -0400

    Add semester number to important dates

--- sql/academics.sql
+++ sql/academics.sql
@@ -8,20 +8,21 @@ GROUP BY term;
 -- Aggregate all lecture, lab and tutorial hours
 DROP VIEW IF EXISTS academics_class_hours;
 CREATE VIEW academics_class_hours AS
-SELECT term,
+SELECT term, semester,
     SUM(CASE WHEN type = "LEC" or type = "PRJ" or type = "SEM" or type = "TUT" or type = "LAB" then duration END) / 60 as academic_hours,
     SUM(CASE WHEN type = "LEC" or type = "PRJ" or type = "SEM" then duration END) / 60 as class_hours,
 	AVG(CASE WHEN type = "LEC" or type = "PRJ" or type = "SEM" then duration END) / 60 as avg_lec_len,
     SUM(CASE WHEN type = "TUT" then duration END) / 60 as tut_hours,
-    SUM(CASE WHEN type = "LAB" then duration END) / 60 as lab_hours
+    SUM(CASE WHEN type = "LAB" then duration END) / 60 as lab_hours,
+    DATEDIFF(MIN(important_dates.end), MIN(important_dates.start)) as length_days
 FROM schedule
 LEFT JOIN important_dates ON schedule.start BETWEEN important_dates.start AND important_dates.end
-GROUP BY term;
+GROUP BY term, semester;
 
 -- Aggregate all interviews received from CECA
 DROP VIEW IF EXISTS academics_coop_interviews;
 CREATE VIEW academics_coop_interviews AS
-SELECT term, COUNT(*) FROM email
+SELECT term, semester, COUNT(*) FROM email
 LEFT JOIN important_dates ON email.date BETWEEN important_dates.start AND important_dates.end
 WHERE email.subject = "uW Pick Interview Slot"
-GROUP BY term;
+GROUP BY term, semester
--- sql/location.sql
+++ sql/location.sql
@@ -24,14 +24,14 @@ GROUP BY start;
 -- Calculate academic event attendance
 DROP VIEW IF EXISTS location_aggregate_event_attendance;
 CREATE VIEW location_aggregate_event_attendance AS
-SELECT term, AVG(present) as attendance FROM location_event_attendance
+SELECT term, semester, AVG(present) as attendance FROM location_event_attendance
 LEFT JOIN important_dates ON location_event_attendance.start BETWEEN important_dates.start AND important_dates.end
-GROUP BY term;
+GROUP BY term, semester;
 
 -- Calculate the number of times I visited home
 DROP VIEW IF EXISTS location_home_visits;
 CREATE VIEW location_home_visits AS
-SELECT term,
+SELECT term, semester,
 	-- Subtract 1 to account for travel on the last day
 	SUM(CASE WHEN name LIKE "home%" THEN 1 END) - 1 as days_at_home
 FROM
@@ -40,4 +40,4 @@ FROM
 	WHERE name IS NOT NULL
 	GROUP BY DATE(datetime), name) as daily_location
 LEFT JOIN important_dates ON DATE(daily_location.date) BETWEEN important_dates.start AND important_dates.end
-GROUP BY term;
+GROUP BY term, semester;
